13f817308005ba25d3f9ad249ccf91640f61d956
Dang Van Dau <dang.van.dau@bpotech.com.vn>
2021-07-09 16:49:28 +0700

    split vendor file config

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,12 @@ module.exports = {
     //Necessary to run npm link https://webpack.js.org/configuration/resolve/#resolve-symlinks
     resolve: {
        symlinks: false
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      }
     }
   },
   transpileDependencies: [
